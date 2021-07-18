5d61e732560f88ce8a8d8e018ec729db0176fbd7
Mubarak <engmubarak48@gmail.com>
2021-07-18 10:37:26 +0000

    initial commit

---
+++ android/app/src/main/kotlin/com/example/meals_APP/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.meals_APP
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../widgets/meal_item.dart';
+
+import '../dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  // const ({ Key? key }) : super(key: key);
+  // final String CategoryId;
+  // final String CategoryTitle;
+
+  // CategoryMealsScreen(this.CategoryId, this.CategoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    final routArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final CategoryTitle = routArgs['title'];
+    final CategoryId = routArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where(
+      (meal) {
+        return meal.categories.contains(CategoryId);
+      },
+    ).toList();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(CategoryTitle),
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return MealItem(
+                title: categoryMeals[index].title,
+                imageUrl: categoryMeals[index].imageUrl,
+                duration: categoryMeals[index].duration,
+                complexity: categoryMeals[index].complexity,
+                affordability: categoryMeals[index].affordability);
+          },
+          itemCount: categoryMeals.length,
+        ));
+  }
+}
